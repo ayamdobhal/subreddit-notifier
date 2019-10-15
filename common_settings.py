372de82e0fffa5a2f740d2fb26ff82d469f1d696
Ayam Dobhal <dobhalayam@gmail.com>
2019-10-16 02:23:45 +0530

    subreddit-notifier:v0.1 release

--- common_settings.py
+++
@@ -1,5 +0,0 @@
-#contains common settings that are not meant to be private/secret.
-
-user_agent = ('android:com.ayamdobhal.subredditnotif:v0.1b'
-		' (by /u/AyamDobhal)')
-
--- main.py
+++ main.py
@@ -8,25 +8,27 @@ from settings import(app_key,app_secret,subreddit_name)
 reddit_client = praw.Reddit(client_id = app_key,
 			    client_secret = app_secret,
 			    user_agent = ('android:'
-					'com.ayam.subredditnotif:v0.1b'
+					'com.ayam.subredditnotif:v0.1'
 					'(by /u/AyamDobhal)')
 				)
 
 subreddit = reddit_client.subreddit(subreddit_name)
-
+print("established connection with the subreddit!")
 
 def post_fetcher(SUBREDDIT):
 	new_submissions = []
-	for submission in subreddit.new(limit=100):
+	for submission in subreddit.new(limit=20):
 		new_submissions.append(submission.id)
 	return new_submissions
 while True:
+	print("starting to fetch submissions.")
 	submissions_1 = post_fetcher(subreddit)
-	time.sleep(175)
+	time.sleep(60)
 	submissions_2 = post_fetcher(subreddit)
 	if submissions_1 == submissions_2:
+		print("No new submissions so far. Is your favorite subreddit dead?")
 		continue
 	else:
-		os.system('''termux-notificafion -t "{} has new 
-				submissions"'''.format(subreddit_name))
+		print("Found new submissions! Sending notification.")
+		os.system('''termux-notification -t "{} has new submissions"'''.format(subreddit_name))
 		continue
